05ee7b78850c6f4d14c59011b96030da32fca763
marcocentofanti <marcocentofanti@hotmail.it>
2026-01-04 21:36:56 +0100

    start prevent default

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-// chiedere all'utente il numero di km che cuole percorrere
-const distance = parseInt(prompt("Quanti km devi percorrere?"))
-console.log(distance)
+//  Raccolta dati
+const userName = document.getElementById("userName")
+const distance = document.getElementById("distance")
+const age = document.getElementById("age")
+const buttonSubmit = document.getElementById("buttonSubmit")
+const buttonReset = document.getElementById("buttonReset")
+const formSection = document.getElementById("formSection")
 
-// chiedere all'utente l'età
-const age = parseInt(prompt("Quanti anni hai?"))
-console.log(age)
-
-// calcolare il prezzo base del biglietto (0.21€/Km)
+//  calcolare il prezzo base del biglietto (0.21€/Km)
 const kmPrice = 0.21
 const ticketPriceStandard = distance * kmPrice
 let ticketPriceFinal
 
-// applicare sconto 20% se età =< 18 anni
-if (age <= 18) {
+formSection.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Funziona!");
+})
+
+//  applicare sconto 20% se età =< 18 anni
+if (age.value == 1) {
   ticketPriceFinal = (ticketPriceStandard * 0.8)
   // applicare sconto 40% se età => 65 anni
-} else if (age >= 65){
+} else if (age.value == 3){
   ticketPriceFinal = (ticketPriceStandard * 0.6)
 } else {
   ticketPriceFinal = ticketPriceStandard
