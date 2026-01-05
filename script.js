a564d9ca6c48c0e52ca75c35f26e52a3ac7e028e
marcocentofanti <marcocentofanti@hotmail.it>
2026-01-05 15:50:34 +0100

    fixed append on ticket field

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 //  Raccolta dati
-const userName = document.getElementById("userName")
-const distance = document.getElementById("distance")
-const age = document.getElementById("age")
-const buttonSubmit = document.getElementById("buttonSubmit")
-const buttonReset = document.getElementById("buttonReset")
-const formSection = document.getElementById("formSection")
+const userNameElement = document.getElementById("userName")
+const distanceElement = document.getElementById("distance")
+const ageElement = document.getElementById("age")
+const buttonSubmitElement = document.getElementById("buttonSubmit")
+const buttonResetElement = document.getElementById("buttonReset")
+const formSectionElement = document.getElementById("formSection")
+
+const ticketNameField = document.getElementById("ticketName")
+const ticketOfferField = document.getElementById("TicketOffer")
+const ticketCarrozzaField = document.getElementById("ticketCarrozza")
+const ticketCodeField = document.getElementById("ticketCode")
+const ticketPriceField = document.getElementById("ticketPrice")
+
+const kmPrice = 0.21
+// let ticketPriceFinal
 
 //  calcolare il prezzo base del biglietto (0.21€/Km)
-const kmPrice = 0.21
-const ticketPriceStandard = distance * kmPrice
-let ticketPriceFinal
+// const ticketPriceStandard = distance * kmPrice
 
-formSection.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Funziona!");
+formSectionElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const userNameValue = userNameElement.value
+    const distanceValue = distanceElement.value
+    const ageValue = ageElement.value
+
+    const newDivEl = document.createElement("div")
+    newDivEl.textContent = userNameValue
+    ticketNameField.append(newDivEl)
 })
 
-//  applicare sconto 20% se età =< 18 anni
-if (age.value == 1) {
-  ticketPriceFinal = (ticketPriceStandard * 0.8)
-  // applicare sconto 40% se età => 65 anni
-} else if (age.value == 3){
-  ticketPriceFinal = (ticketPriceStandard * 0.6)
-} else {
-  ticketPriceFinal = ticketPriceStandard
-}
+
+function calcoloOfferta(){
+  //  applicare sconto 20% se età =< 18 anni
+  if (age.value == 1) {
+   return (ticketPriceStandard * 0.8)
+    // applicare sconto 40% se età => 65 anni
+  } else if (age.value == 3){
+     return (ticketPriceStandard * 0.6)
+  } else {
+    return ticketPriceStandard
+  }
   //stampare output con solo 2 decimali
   console.log(`il costo del tuo biglietto è ${ticketPriceFinal.toFixed(2)}€`);
-
+}
+  
   // 100km, 10 anni => prezzo corretto:  €16.80
   // 100km, 70 anni => prezzo corretto: €12.60
